afba716d5a9e942289a8aa457c575a91df603457
nafuente <nery11f@gmail.com>
2021-12-29 23:16:23 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+import {App} from "./App.jsx"
+
+
+
+function Componente({titulo,children}){
+    
+    // destructurar
+    //const {titulo,contenido}=props
+    return (
+        <div className="container">
+            <h1>{titulo}</h1>
+            <div>{children}</div>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App/>,
+    document.getElementById("root")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
